69b04d49b58dda43bd1e934d02d073f7d6d5860c
Clackroe <clackroe@gmail.com>
2024-05-06 21:08:21 -0400

    feat: Display keys

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,20 @@ import Image from "next/image";
 import Person from "./components/person";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        THIS IS IN THE DIVv
-      </div>
-      <Person />
-    </main>
-  );
+
+    const NUM_PEOPLE = 3;
+    const renderPeople = () => {
+        const people = [];
+        for (let i = 0; i < NUM_PEOPLE; i++) {
+            people.push(<Person key={i} />);
+        }
+        return people;
+    };
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                {renderPeople()}
+            </div>
+        </main>
+    );
 }
